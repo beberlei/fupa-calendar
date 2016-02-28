bd3726aa0300abab3f99f49d45f50c4642c2cc89
Benjamin Eberlei <kontakt@beberlei.de>
2016-02-28 18:17:44 +0100

    Filter away matches and fix timezone

--- src/FupaCalendar/Generator.php
+++ src/FupaCalendar/Generator.php
@@ -33,6 +33,7 @@ class Generator
 
             $teamName = trim($crawler->filter('.head_line h1')->text());
             $rows = $crawler->filter('.rows .row');
+            $utcZone = new \DateTimeZone('UTC');
 
             foreach ($rows as $element) {
                 $crawler = new Crawler($element);
@@ -43,14 +44,20 @@ class Generator
                     $awayTeam = $crawler->filter('.name_gast')->text();
                     $isHomeGame = strpos($homeTeam, $teamName) !== false;
 
+                    if (!$isHomeGame) {
+                        continue;
+                    }
+
                     if (strpos($date, ", ") === false) {
                         throw new \InvalidArgumentException("Cannot find the right date format.");
                     }
 
                     list ($day, $date) = explode(', ', $date);
                     $start = DateTime::createFromFormat('d.m.Y H:i', sprintf('%s %s', $date, $time));
+                    $start->setTimeZone($utcZone);
                     $end = clone $start;
                     $end = $start->modify('+105 minute');
+                    $end->setTimeZone($utcZone);
 
                     $vEvent = new Event();
                     $vEvent->setDtStart($start)->setDtEnd($end)->setSummary(sprintf('Heimspiel: %s vs %s', $homeTeam, $awayTeam));
